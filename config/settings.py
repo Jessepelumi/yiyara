f468185977658efe51677ca0eb28ff0d553b10e2
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-08-09 05:10:22 +0100

    feat: load internal auth secret from the env file

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
+INTERNAL_AUTH_SECRET = os.getenv('INTERNAL_AUTH_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
